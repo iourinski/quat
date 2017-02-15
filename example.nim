2702fa364ce8cd47cc6ee087deb13a8d7fc4f9bc
dmitri.iourinski <d.iourinski@gmail.com>
2017-02-15 19:36:23 +0300

    some commit

--- example.nim
+++ example.nim
@@ -1,10 +1,29 @@
-import jester, asyncdispatch, htmlgen
+import 
+  asyncdispatch, 
+  events_handling, 
+  hashes, 
+  htmlgen, 
+  jester, 
+  pudgeclient
+let pudge = newClient()
 
 routes:
   get "/":
-    #attachment "public/FF4D00-0.8.png"
+    #[try: 
+      var str = recordEvent("HITS", request, pudge)  
+    except PudgeError:
+      resp Http500, "Can't record"  
+    ]#
+    resp Http200, "OK"
+      
+  get "/test":
+    
+    #let pudge  = newClient("127.0.0.1", 11213)  
     var str = ""
-    for x in request.headers.pairs:
-        str = str & x.key & ": " & x.value & "<br/>"
-    resp repr(str)
+    try:
+      str = demonstrateEvent("HITS", request, pudge)      
+    except PudgeError:
+      resp Http500, "can't record"
+    resp $str
+
 runForever()
